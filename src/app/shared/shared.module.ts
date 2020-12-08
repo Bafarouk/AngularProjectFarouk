99957111278023bab5377d76ca1bb3628d25efa6
Bafarouk <44127908+Bafarouk@users.noreply.github.com>
2020-12-09 00:10:19 +0100

    Weather Widget Api

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './layouts/home/home.component';
 import { RouterModule } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
+import { WeatherWidgetComponent } from './components/weather-widget/weather-widget.component';
 
 
 
 @NgModule({
-  declarations: [HomeComponent,HeaderComponent],
+  declarations: [HomeComponent,HeaderComponent, WeatherWidgetComponent],
   imports: [
     CommonModule,
     RouterModule
